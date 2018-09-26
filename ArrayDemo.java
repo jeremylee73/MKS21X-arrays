5a8b5171cf1fb5ea3871de8ebce72f780c8f0b88
Jeremy Lee <jlee00@stuy.edu>
2018-09-26 18:34:54 -0400

    Added ArrayDemo.java template

---
+++ ArrayDemo.java
@@ -0,0 +1,25 @@
+public class ArrayDemo {
+  // public static void printArray(int[]ary){
+  //
+  // }
+
+  // public static void printArray(int[][]ary){
+  //   return null;
+  // }
+  //
+  // public static int countZeros2D(int[][] nums){
+  //   return null;
+  // }
+  //
+  // public static void fill2D(int[][] vals){
+  //   return null;
+  // }
+  //
+  // public static int[][] fill2DCopy(int[][] vals){
+  //
+  // }
+
+  public static void main(String[]args) {
+    System.out.println("Hello World");
+  }
+}
